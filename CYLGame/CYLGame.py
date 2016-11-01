ddc82f5e8d4af9d584c2e302646ba4fa0470af25
Jonathan Beaulieu <123.jonathan@gmail.com>
2016-11-01 14:36:21 -0500

    Cleaned up and added some documentation.

--- CYLGame/CYLGame.py
+++ CYLGame/CYLGame.py
@@ -2,10 +2,10 @@ import tcod as libtcod
 import tdl
 import tempfile
 import os.path
-# import tcod_to_tdl as libtcod
 
 TCOT_ROOT_CONSOLE = None
 TDL_ROOT_CONSOLE = None
+
 
 def data_file(filename):
     resource_path = os.path.join(os.path.split(__file__)[0], os.path.pardir, "data", filename)
@@ -30,6 +30,11 @@ class CYLGame(object):
     CHAR_SET = "terminal8x8_gs_ro.png"
 
     def is_running(self):
+        """This is how the game runner knows if the game is over.
+
+        Returns:
+            bool: True if the game should still be play. False otherwise
+        """
         raise Exception("Not implemented!")
 
     def get_vars_for_bot(self):
@@ -40,6 +45,11 @@ class CYLGame(object):
         raise Exception("Not implemented!")
 
     def handle_key(self, key):
+        """This is how the game runner knows if the game is over.
+
+        Returns:
+            bool: True if the game should still be play. False otherwise
+        """
         raise Exception("Not implemented!")
 
     def draw_screen(self, libtcod, console):
@@ -55,36 +65,31 @@ class CYLGameRunner(object):
 
         self.kill = False
 
-        keys = [chr(x) for x in range(ord("A"), ord("Z")+1)] + \
-               [chr(x) for x in range(ord("a"), ord("z")+1)] + \
-               [chr(x) for x in range(ord("0"), ord("9")+1)]
+        keys = [chr(x) for x in range(ord("A"), ord("Z") + 1)] + \
+               [chr(x) for x in range(ord("a"), ord("z") + 1)] + \
+               [chr(x) for x in range(ord("0"), ord("9") + 1)]
 
         self.key_consts = {}
         for c in keys:
-            self.key_consts["key_"+c] = ord(c)
+            self.key_consts["key_" + c] = ord(c)
 
         self.dir_consts = {"north": ord("w"), "south": ord("s"), "west": ord("a"), "east": ord("d")}
-        # for i in range(10):
-        #     self.key_consts["NUM" + str(i)] = ord(str(i))
 
     def run(self):
         global TDL_ROOT_CONSOLE
         self.kill = False
         if not self.bot:
             if not TDL_ROOT_CONSOLE:
-                # TCOT_ROOT_CONSOLE = libtcod.console_init_root(self.game.SCREEN_WIDTH, self.game.SCREEN_HEIGHT, self.game.GAME_TITLE)
                 tdl.setFont(data_file("fonts/" + self.game_class.CHAR_SET))
-                TDL_ROOT_CONSOLE = tdl.init(self.game_class.SCREEN_WIDTH, self.game_class.SCREEN_HEIGHT, self.game_class.GAME_TITLE)
-                #libtcod.console_set_custom_font(data_file("fonts/terminal16x16_gs_ro.png"), libtcod.FONT_LAYOUT_ASCII_INROW)
+                TDL_ROOT_CONSOLE = tdl.init(self.game_class.SCREEN_WIDTH, self.game_class.SCREEN_HEIGHT,
+                                            self.game_class.GAME_TITLE)
         else:
             self.vars = {}
             if not TDL_ROOT_CONSOLE:
-                # TCOT_ROOT_CONSOLE = libtcod.console_init_root(0, 0, self.game.GAME_TITLE)
                 TDL_ROOT_CONSOLE = tdl.init(0, 0)
 
         self.game = self.game_class()
 
-        # self.console = libtcod.console_new(self.game.SCREEN_WIDTH, self.game.SCREEN_HEIGHT)
         self.console = tdl.Console(self.game.SCREEN_WIDTH, self.game.SCREEN_HEIGHT)
         self.screen_cap = []
         while self.game.is_running() and not self.kill:
@@ -105,35 +110,19 @@ class CYLGameRunner(object):
         arr = []
         for i in range(y):
             arr += [map(ord, chars[i::x])]
-        # fp.close()
-        # tf.delete
         return arr
-        # arr = []
-        # for j in range(self.game.SCREEN_HEIGHT):
-        #     arr += [[]]
-        #     for i in range(self.game.SCREEN_WIDTH):
-        #         # char = libtcod.console_get_char(self.console, i, j)
-        #         char = self.console.get_char(i, j)[0]
-        #         arr[j] += [char]
-        # return arr
 
     def __run_user(self):
         self.game.draw_screen(libtcod, self.console.tcod_console)
-        # libtcod.console_blit(self.console, 0, 0, self.game.SCREEN_WIDTH, self.game.SCREEN_HEIGHT, TCOT_ROOT_CONSOLE, 0, 0)
-        # libtcod.console_flush()
         TDL_ROOT_CONSOLE.blit(self.console, 0, 0, self.game.SCREEN_WIDTH, self.game.SCREEN_HEIGHT, 0, 0)
         tdl.flush()
 
-        # key = libtcod.console_wait_for_keypress(True)
         key = tdl.event.key_wait()
         if key.char:
-            # c = chr(key.c)
             self.game.handle_key(key.char)
 
     def __run_bot(self):
         self.game.draw_screen(libtcod, self.console.tcod_console)
-        # libtcod.console_blit(self.console, 0, 0, self.game.SCREEN_WIDTH, self.game.SCREEN_HEIGHT, TCOT_ROOT_CONSOLE, 0, 0)
-        # libtcod.console_flush()
         self.screen_cap += [self.get_screen_array()]
 
         self.vars.update(self.game.get_vars_for_bot())
