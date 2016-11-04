883b59762e91a143ef8c02a20ca3585aa7ac802d
Jonathan Beaulieu <123.jonathan@gmail.com>
2016-11-04 15:01:33 -0500

    Updated Database to prevent race contitions.

--- CYLGame/Database.py
+++ CYLGame/Database.py
@@ -3,47 +3,60 @@ import ujson
 import random
 
 
-# TODO(derpferd): make thread safe in future.
+# TODO(derpferd): Use the move function to prevent RACE on files
 class GameDB(object):
     TOKEN_LEN = 8
 
     def __init__(self, game_dir):
         self.game_dir = game_dir
         self.data_dir = os.path.join(self.game_dir, "data")
-        self.meta_fn = os.path.join(self.game_dir, "metadata.json")
+        self.schools_dir = os.path.join(self.game_dir, "schools")
         self.__load()
 
     def __load(self):
-        is_new = False
+        # is_new = False
         if not os.path.exists(self.game_dir):
-            is_new = True
+            # is_new = True
             os.mkdir(self.game_dir)
         if not os.path.exists(self.data_dir):
-            is_new = True
+            # is_new = True
             os.mkdir(self.data_dir)
-        if not os.path.exists(self.meta_fn):
-            is_new = True
+        if not os.path.exists(self.schools_dir):
+            # is_new = True
+            os.mkdir(self.schools_dir)
 
-        if is_new:
-            self.schools = {}
-            self.tokens = {}
-            self.__save()
-        else:
-            obj = ujson.load(open(self.meta_fn, "r"))
-            assert "schools" in obj
-            assert "tokens" in obj
-            self.schools = obj["schools"]
-            self.tokens = obj["tokens"]
+        # if is_new:
+        #
+        #     # self.schools = {}
+        #     # self.tokens = {}
+        #     # self.__save()
+        # else:
+        #     obj = ujson.load(open(self.meta_fn, "r"))
+        #     assert "schools" in obj
+        #     assert "tokens" in obj
+        #     self.schools = obj["schools"]
+        #     self.tokens = obj["tokens"]
 
-    def __save(self):
-        ujson.dump({"schools": self.schools, "tokens": self.tokens}, open(self.meta_fn, "w"))
+    # def __save(self):
+    #     ujson.dump({"schools": self.schools, "tokens": self.tokens}, open(self.meta_fn, "w"))
+
+    def __get_user_tokens(self):
+        return os.listdir(self.data_dir)
+
+    def __get_school_tokens(self):
+        return os.listdir(self.schools_dir)
+
+    def __get_school_user_tokens(self, school_tk):
+        if self.is_school_token(school_tk):
+            return os.listdir(self.__get_dir_for_token(school_tk, "tokens"))
+        return []
 
     def __get_new_token(self, tokens=None, prefix=""):
 
         def new_token():
             return prefix + "".join([random.choice("0123456789ABCDEF") for _ in range(self.TOKEN_LEN)])
         if not tokens:
-            tokens = self.tokens
+            tokens = self.__get_user_tokens()
         token = new_token()
         while token in tokens:
             token = new_token()
@@ -60,31 +73,43 @@ class GameDB(object):
             fns = [fns]
         else:
             assert isinstance(fns, list)
-        return os.path.join(self.data_dir, token, *fns)
+        if self.is_user_token(token):
+            return os.path.join(self.data_dir, token, *fns)
+        elif self.is_school_token(token):
+            return os.path.join(self.schools_dir, token, *fns)
+        return None
 
     def is_school_token(self, token):
         if len(token) > 0 and token[0] == "S":
             # It is a school token
-            return token in self.schools
+            return token in self.__get_school_tokens()
         return False
 
     def is_user_token(self, token):
-        return token in self.tokens
+        return token in self.__get_user_tokens()
 
-    def get_new_token(self, school_tk, name=""):
+    def get_new_token(self, school_tk):
         # Is name needed?
-        assert school_tk in self.schools
+        assert self.is_school_token(school_tk)
         token = self.__get_new_token()
-        self.schools[school_tk]["tokens"] += [token]
-        self.tokens[token] = {"name": name}
-        os.mkdir(self.__get_dir_for_token(token))
-        self.__save()
+
+        # Touch the file
+        with open(self.__get_dir_for_token(school_tk, ["tokens", token]), "w") as fp:
+            pass
+
+        # Create token dir
+        os.mkdir(os.path.join(self.data_dir, token))
         return token
 
     def add_new_school(self, name=""):
-        token = self.__get_new_token(self.schools.keys(), prefix="S")
-        self.schools[token] = {"name": name, "tokens": []}
-        self.__save()
+        token = self.__get_new_token(self.__get_school_tokens(), prefix="S")
+
+        os.mkdir(self.__get_dir_for_token(token))
+        os.mkdir(self.__get_dir_for_token(token, "tokens"))
+
+        with open(self.__get_dir_for_token(token, "name")) as fp:
+            fp.write(name)
+
         return token
 
     def save_code(self, token, code):
@@ -97,6 +122,17 @@ class GameDB(object):
         assert os.path.exists(self.__get_dir_for_token(token))
         with open(self.__get_dir_for_token(token, "code.lp"), "w") as fp:
             fp.write(code)
+
+    def save_name(self, token, name):
+        """Save a user's name under their token.
+
+        Args:
+            token (str): The user's token.
+            name (str): The user's name.
+        """
+        assert os.path.exists(self.__get_dir_for_token(token))
+        with open(self.__get_dir_for_token(token, "name"), "w") as fp:
+            fp.write(name)
 
     def save_avg_score(self, token, score):
         """Save a user's average score.
@@ -116,6 +152,17 @@ class GameDB(object):
         else:
             return None
 
+    def get_name(self, token):
+        if self.is_user_token(token):
+            if os.path.exists(self.__get_dir_for_token(token, "name")):
+                with open(self.__get_dir_for_token(token, "name"), "r") as fp:
+                    return fp.read()
+        elif self.is_school_token(token):
+            if os.path.exists(self.__get_dir_for_token(token, "name")):
+                with open(self.__get_dir_for_token(token, "name"), "r") as fp:
+                    return fp.read()
+        return None
+
     def get_avg_score(self, token):
         if os.path.exists(self.__get_dir_for_token(token, "avg_score")):
             with open(self.__get_dir_for_token(token, "avg_score"), "r") as fp:
@@ -123,17 +170,21 @@ class GameDB(object):
         else:
             return None
 
-    def get_name(self, token):
-        if self.is_user_token(token):
-            return self.tokens[token]["name"]
-        elif self.is_school_token(token):
-            return self.schools[token]["name"]
+    # def get_name(self, token):
+    #     if self.is_user_token(token):
+    #         return self.
+    #         return self.tokens[token]["name"]
+    #     elif self.is_school_token(token):
+    #         return self.schools[token]["name"]
 
     def get_school_for_token(self, token):
-        for school in self.schools:
-            if token in self.schools[school]["tokens"]:
+        for school in self.__get_school_tokens():
+            if token in self.__get_school_user_tokens(school):
                 return school
         return None
 
     def get_tokens_for_school(self, school_tk):
-        return self.schools[school_tk]["tokens"]
+        return self.__get_school_user_tokens(school_tk)
+
+    def get_school_tokens(self):
+        return self.__get_school_tokens()
---
+++ game_db_editor.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+from __future__ import print_function
+from builtins import input
+import os
+from CYLGame.Database import GameDB
+
+gamedb = None
+cur_school = None
+
+
+def clear():
+    os.system("clear")
+
+
+def get_input(prompt="", validator=lambda x: x, error_msg=""):
+    inp = None
+    err = ""
+    while not inp or not validator(inp):
+        print(err)
+        inp = input(prompt)
+    return inp
+
+
+def print_menu(options=[], title=""):
+    clear()
+    print(title)
+    for index, option in enumerate(options):
+        print(str(index+1)+":", option)
+    return int(get_input("Select an item: ", lambda x: int(x) in range(1,len(options)+1), "Invalid Selection. Try Again."))-1
+
+
+def select_school():
+    global gamedb, cur_school
+    options = []
+    options_to_tokens = {}
+    for i, school_tk in enumerate(gamedb.get_school_tokens()):
+        options += [gamedb.get_name(school_tk)]
+        options_to_tokens[i] = school_tk
+    cur_school = options_to_tokens[print_menu(options, "Select School")]
+    print("Current School Set to:", cur_school)
+
+
+def get_new_tokens():
+    global gamedb, cur_school
+    clear()
+    count = int(get_input("How many tokens would you like: ", lambda x: x.isdigit(), "Please enter a number."))
+    clear()
+    print("New tokens")
+    for _ in range(count):
+        print(gamedb.get_new_token(cur_school))
+
+    get_input("Copy tokens then enter any input: ")
+
+
+def get_main_menu_options():
+    global cur_school
+    options = ["Add New School", "Select School"]
+    if cur_school is not None:
+        options += ["Get new Tokens"]
+    return options + ["Quit"]
+
+
+def get_main_menu_title():
+    global cur_school
+    title = "Main Menu"
+    if cur_school is not None:
+        title += "\n\nSelected School: "+cur_school
+    return title
+
+
+def main():
+    global gamedb
+    print("Welcome to the GameDB Editor")
+    game_path = os.path.abspath(get_input("Your current dir is '"+os.path.abspath(os.curdir)+"'\nEnter path to game dir: ", lambda x: os.path.exists(x), error_msg="Invalid Game Directory. Try Again."))
+    gamedb = GameDB(game_path)
+    option = ""
+    while option != "Quit":
+        options = get_main_menu_options()
+        option = options[print_menu(options, get_main_menu_title())]
+        print("You selected:", option)
+        if option == "Select School":
+            select_school()
+        elif option == "Get new Tokens":
+            get_new_tokens()
+
+
+if __name__ == '__main__':
+    main()
